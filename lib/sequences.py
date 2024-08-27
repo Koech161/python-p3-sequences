205e5b58179a44303ed2d91708c02ffdb6c59ef5
Abel Koech <koechabel161@gmail.com>
2024-08-27 18:46:22 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    #pass
+    if length < 0:
+      print('Invalid length')
+      return
+    elif length == 0:
+         print([])
+         return
+    elif length == 1:
+         print([0])
+         return
+      
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    print(fibonacci_sequence)  
+print_fibonacci(2)  
